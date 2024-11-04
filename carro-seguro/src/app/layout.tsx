285b9497c8155a60b0cfa86cb96f39a11d163b1f
felipesora <felipeusora@gmail.com>
2024-11-04 17:50:15 -0300

    arrumando pasta de pagina-inicial

--- carro-seguro/src/app/layout.tsx
+++ carro-seguro/src/app/layout.tsx
@@ -1,9 +1,11 @@
 "use client"; 
 
-import Cabecalho from '@/components/Cabecalho/Cabecalho/Cabecalho';
+
+import Cabecalho from '@/componentes/Cabecalho/Cabecalho';
 import RootLayout from './RootLayout';
 import { usePathname } from "next/navigation"; 
-import Rodape from '@/components/Rodape/Rodape/Rodape';
+import Rodape from '@/componentes/Rodape/Rodape';
+
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
